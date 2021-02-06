23f19c0c6fc18e1a231fd4413edb14acc1261db4
詹周东 <zhanzhoudong@adpanshi.com>
2021-02-06 13:55:56 +0800

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <img src={logo} className="App-logo" alt="logo" /><p>Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,6 +14,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
+          
           Learn React
         </a>
       </header>
